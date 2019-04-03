7b638b5231774d135d92299a6cbee1ec1e4db012
Gery Wibowo <akun.gery@gmail.com>
2019-04-03 22:06:39 +0700

    state management for nominal, currency, & rates

--- src/store.ts
+++ src/store.ts
@@ -5,13 +5,35 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    baseCurrency: 'USD',
-    baseAmount: 10
+    currency: 'USD',
+    nominal: 10,
+    rates: null
   },
   mutations: {
-
+    setCurrency (state, action) {
+      state.currency = action
+    },
+    setNominal (state, action) {
+      state.nominal = action
+    },
+    setRates (state, action) {
+      state.rates = action
+    }
   },
   actions: {
-
+    setCurrency ({ commit, state }) {
+      commit('setCurrency', state.currency)
+    },
+    setNominal ({ commit, state }) {
+      commit('setNominal', state.nominal)
+    },
+    setRates ({ commit, state }) {
+      commit('setRates', state.rates)
+    }
+  },
+  getters: {
+    getCurrency: state => state.currency,
+    getNominal: state => state.nominal,
+    getRates: state => state.rates
   }
 })
