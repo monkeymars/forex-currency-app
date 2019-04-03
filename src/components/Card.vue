4c7a4977939022892632d968b6cd34e4ee354cb6
Gery Wibowo <akun.gery@gmail.com>
2019-04-04 01:29:27 +0700

    remove card currency

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="ui fluid card">
     <div class="content">
-      <span class="ui large header right floated" style="margin:0;">
-        {{ currencyFormatter(cardCurrency, (rate * activeNominal)) }}
-      </span>
+      <span
+        v-html="currencyFormatter(cardCurrency, (rate * activeNominal))"
+        class="ui large header right floated"
+        style="margin:0;"
+      />
       <div class="ui large header left floated" style="margin:0;">{{ cardCurrency }}</div>
       <span class="description left floated">{{ cardCurrency }} - {{ currencyName }}</span>
     </div>
 
     <div class="extra content">
-      <span class="left floated like"> 1 {{ activeCurrency }} = {{ cardCurrency }} {{ currencyFormatter(cardCurrency, rate) }}</span>
-      <button class="ui mini basic white button right floated star">
+      <span class="left floated like">
+        <label v-html="'1 ' + activeCurrency + ' = ' + cardCurrency + ' ' + currencyFormatter(cardCurrency, rate)"/>
+      </span>
+      <button @click="removeCard(cardCurrency)" class="ui mini basic white button right floated star">
         <i class="ui red minus icon" style="margin:0;"></i>
       </button>
     </div>
@@ -21,6 +25,14 @@
 export default {
   name: 'Card',
   methods: {
+    removeCard (currencyId) {
+        let cards = this.$store.getters['getCards']
+        cards = cards.filter((item) => {
+          return item.cardCurrency !== currencyId
+        })
+        this.$store.commit('setCards', cards)
+    },
+
     currencyFormatter (currency, nominal) {
       const formatter = new Intl.NumberFormat({
         style: 'currency',
@@ -31,6 +43,7 @@ export default {
     },
   },
   props: [
+    'cardIndex',
     'currencyName',
     'activeNominal',
     'activeCurrency',
