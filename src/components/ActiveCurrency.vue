c6efba2c1e19a37e2c6c02d679beadc4a64d3703
Gery Wibowo <akun.gery@gmail.com>
2019-04-03 22:05:38 +0700

    store values on active currency component

--- src/components/ActiveCurrency.vue
+++ src/components/ActiveCurrency.vue
@@ -2,16 +2,32 @@
   <div class="ui two column centered grid">
     <div class="ten wide column">
       <div class="column">
-        <i class="ui grey tiny header">USD - United States Dollars</i><br>
+        <i class="ui grey tiny header">
+          {{ selected_currency }} - {{ currencyName }}
+        </i>
+        <br>
         <div class="ui input">
-          <select class="ui medium header selection dropdown select-currency">
-            <option value="all">USD</option>
-            <option value="all">EUR</option>
-            <option value="all">IDR</option>
+          <select
+            v-model="selected_currency"
+            class="ui medium header selection dropdown select-currency"
+            @change="changeCurrency(selected_currency)"
+          >
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="IDR">IDR</option>
           </select>
         </div>
-        <div class="ui medium header transparent input right floated" style="margin-top:10px;min-width:200px;">
-          <input class="ui medium header" value="10" min="1" type="number" style="text-align: right;">
+        <div
+          class="ui medium header transparent input right floated"
+          style="margin-top:10px;min-width:200px;"
+        >
+          <input
+            class="ui medium header"
+            v-model="nominal"
+            min="1"
+            type="number"
+            style="text-align: right;"
+          >
         </div>
       </div>
       <div class="column">
@@ -22,16 +38,59 @@
 </template>
 
 <script>
+import {
+  getCurrecyName,
+  getCurrecies,
+  getBaseRates
+} from '@/services/rates.service'
+
 export default {
   name: 'ActiveCurrency',
-  mounted () {}
+  data () {
+    return {
+      selected_currency: 'USD',
+      currencyName: 'United States Dollars'
+    }
+  },
+  computed: {
+    nominal: {
+      get () {
+        return this.$store.getters['getNominal']
+      },
+      set (value) {
+        return this.$store.commit('setNominal', value)
+      }
+    }
+  },
+  methods: {
+    changeCurrency (value) {
+      // set active currency
+      this.$store.commit('setCurrency', value)
+
+      // get currency name
+      getCurrecyName(value, (res) => {
+        this.currencyName = res
+      })
+
+      // get base rates
+      getBaseRates(value, (result) => {
+        this.$store.commit('setRates', result.rates)
+      })
+
+      console.log(
+        this.$store.getters['getCurrency'],
+        this.$store.getters['getNominal'],
+        this.$store.getters['getRates']
+      )
+    }
+  }
 }
 </script>
 
 <style>
-    .select-currency {
-        border:none !important;
-        padding:0 !important;
-        min-width: 65px !important;
-    }
+  .select-currency {
+    border: none !important;
+    padding: 0 !important;
+    min-width: 65px !important;
+  }
 </style>
